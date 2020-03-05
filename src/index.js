a19bc401bc39404451a41ecd0cc72912fc12a91e
stldo <stldo.dev@outlook.com>
2020-03-05 00:22:22 -0300

    ESM; Options can be set only with an object; String.normalize instead of unidecode.

--- src/index.js
+++ src/index.js
@@ -1,207 +1,124 @@
-const unidecode = require('unidecode')
-
-/* TODO v3
- * - Set default transformers as exports
- * - ESM with multiple exports
- * - Deprecate defaultTransformers
- * - Deprecate parseOptions, use a config object instead
- * - Deprecate null as separator
- * - Add support to NFKD normalize, make unidecode an option (full?)
- * - Browser support
- * - Add sentence case transformer
- * - Supported characters:
- *   - default: ALPHA / DIGIT / "-" / "." / "_" / "~"
- *   - path: ALPHA / DIGIT / "-" / "." / "_" / "~" / "!" / "$" / "&" /
- *           "'" / "(" / ")" / "*" / "+" / "," / ";" / "=" / "@"
- *     - ":" is non-zero-length segment without any colon, allow it?
- *   - query: ALPHA / DIGIT / "-" / "." / "_" / "~" / "!" / "$" / "'" / "(" /
- *            ")" / "*" / "+" / "," / ";" / "=" / "@" / "/" / "?"
- *     - "&" is commonly used to separate key/value pairs, allow it?
- *   - hash: ALPHA / DIGIT / "-" / "." / "_" / "~" / "!" / "$" / "&" / "'" /
- *           "(" / ")" / "*" / "+" / "," / ";" / "=" / "@" / "/" / "?"
- */
-
+const COMBINING_CHARS = /[\u0300-\u036F\u1AB0-\u1AFF\u1DC0-\u1DFF]+/g
 const INVALID_SEPARATOR = /[^-._~]/
+
 const CONVERT = /[A-Za-z\d]+/g
 const REVERT = /[^-._~]+/g
 
-const BASE = '(?:[a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))'
-const CONVERT_CAMELCASE = new RegExp(`[A-Za-z0-9]*?${BASE}|[A-Za-z0-9]+`, 'g')
-const REVERT_CAMELCASE = new RegExp(`[^-._~]*?${BASE}|[^-._~]+`, 'g')
-const REVERT_CAMELCASE_WITH_EMPTY_SEPARATOR = new RegExp(`.*?${BASE}|.+`, 'g')
+const CCASE = '(?:[a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))'
+const CONVERT_CAMELCASE = new RegExp(`[A-Za-z0-9]*?${CCASE}|[A-Za-z0-9]+`, 'g')
+const REVERT_CAMELCASE = new RegExp(`[^-._~]*?${CCASE}|[^-._~]+`, 'g')
+const REVERT_CAMELCASE_WITH_EMPTY_SEPARATOR = new RegExp(`.*?${CCASE}|.+`, 'g')
 
-/**
-* Check and return validated options
-*/
+/* Parse options */
+
 function parseOptions(options) {
-  let camelCase
-  let separator
-  let transformer
+  const result = {}
 
-  if (2 === options.length) {
-    [separator, transformer] = options
-    validate({ separator, transformer })
-  } else if (1 === options.length) {
-    const option = options[0]
-    if (false === option || 'function' === typeof option) {
-      transformer = option /* Don't validate */
-    } else if (defaultTransformers[option]) { /* TODO Remove in v3 */
-      transformer = option
-      validate({ transformer }) /* Throw deprecation error */
-    } else if (typeof option === 'string') {
-      separator = option
-      validate({ separator })
-    } else if (option != null) {
-      camelCase = option.camelCase
-      separator = option.separator
-      transformer = option.transformer
-      validate({ camelCase, separator, transformer })
+  if (options.hasOwnProperty('camelCase')) {
+    const value = options.camelCase
+
+    if (typeof value !== 'boolean') {
+      throw new Error(`camelCase must be a boolean: "${value}".`)
     }
+
+    result.camelCase = value
   }
 
-  if (defaultTransformers[transformer]) { /* Remove in v3.0.0 */
-    transformer = defaultTransformers[transformer]
+  if (options.hasOwnProperty('separator')) {
+    const value = options.separator
+
+    if (typeof value !== 'string') {
+      throw new Error(`separator must be a string: "${value}".`)
+    } else if (INVALID_SEPARATOR.test(value)) {
+      const error = value.match(INVALID_SEPARATOR)[0]
+      throw new Error(`separator has an invalid character: "${error}".`)
+    }
+
+    result.separator = value
   }
 
-  return {
-    camelCase,
-    separator: separator == null ? void 0 : separator,
-    transformer
+  if (options.hasOwnProperty('transformer')) {
+    const value = options.transformer
+
+    if (value !== false && typeof value !== 'function') {
+      throw new Error(`transformer must be false or a function: "${value}".`)
+    }
+
+    result.transformer = value
   }
+
+  return result
 }
 
-/**
-* Validate options
-*/
-function validate({ camelCase, separator, transformer }) {
-  if (camelCase !== undefined) {
-    if (typeof camelCase !== 'boolean') {
-      throw new Error(`camelCase must be a boolean: "${camelCase}".`)
-    }
-  }
+/* Builtin transformers */
 
-  if (separator != null) {
-    if ('string' !== typeof separator) {
-      throw new Error(`separator must be a string: "${separator}".`)
-    } else if (INVALID_SEPARATOR.test(separator)) {
-      throw new Error(`separator has invalid characters: "${separator}".`)
-    }
-  }
-
-  if (transformer != null) {
-    if (transformer !== false && typeof transformer !== 'function') {
-      if (typeof transformer === 'string' && defaultTransformers[transformer]) {
-        throw new Error('Using transformer name as string was deprecated')
-         /* TODO Remove in v3 */
-      } else {
-        throw new Error(`transformer must be a function: "${transformer}".`)
-      }
-    }
-  }
+export const LOWERCASE_TRANSFORMER = (fragments, separator) => {
+  return fragments.join(separator).toLowerCase()
 }
 
-class UrlSlug {
-
-  static LOWERCASE_TRANSFORMER(fragments, separator) {
-    return fragments.join(separator).toLowerCase()
-  }
-
-  static UPPERCASE_TRANSFORMER(fragments, separator) {
-    return fragments.join(separator).toUpperCase()
-  }
-
-  static TITLECASE_TRANSFORMER(fragments, separator) {
-    return fragments.map(fragment => {
-      return fragment.charAt(0).toUpperCase() + fragment.slice(1).toLowerCase()
-    }).join(separator)
-  }
-
-  /**
-  * Creates a new instance of url-slug
-  */
-  constructor(...options) {
-    const {
-      camelCase = true,
-      separator = '-',
-      transformer = UrlSlug.LOWERCASE_TRANSFORMER
-    } = parseOptions(options)
-
-    this.camelCase = camelCase
-    this.separator = separator
-    this.transformer = transformer
-  }
-
-  /**
-  * Converts a string into a slug
-  */
-  convert(string, ...options) {
-    const {
-      camelCase = this.camelCase,
-      separator = this.separator,
-      transformer = this.transformer,
-    } = parseOptions(options)
-
-    const fragments = unidecode(String(string))
-      .match(camelCase ? CONVERT_CAMELCASE : CONVERT)
-
-    if (!fragments) {
-      return ''
-    }
-
-    return transformer
-      ? transformer(fragments, separator)
-      : fragments.join(separator)
-  }
-
-  /**
-  * Reverts a slug back to a string
-  */
-  revert(slug, ...options) {
-    const {
-      camelCase = false,
-      separator = null,
-      transformer = false
-    } = parseOptions(options)
-
-    let fragments
-    slug = String(slug)
-
-    /* Determine which method will be used split the slug */
-
-    if ('' === separator) {
-      fragments = slug.match(REVERT_CAMELCASE_WITH_EMPTY_SEPARATOR)
-    } else if ('string' === typeof separator) {
-      fragments = slug.split(separator)
-    } else {
-      fragments = slug.match(camelCase ? REVERT_CAMELCASE : REVERT)
-    }
-
-    if (!fragments) {
-      return ''
-    }
-
-    return transformer ? transformer(fragments, ' ') : fragments.join(' ')
-  }
-
+export const TITLECASE_TRANSFORMER = (fragments, separator) => {
+  return fragments.map(fragment => {
+    return fragment.charAt(0).toUpperCase() + fragment.slice(1).toLowerCase()
+  }).join(separator)
 }
 
-/**
-* Builtin transformers
-*/
-const defaultTransformers = { /* TODO Deprecate in v3 */
-  lowercase: UrlSlug.LOWERCASE_TRANSFORMER,
-  uppercase: UrlSlug.UPPERCASE_TRANSFORMER,
-  titlecase: UrlSlug.TITLECASE_TRANSFORMER,
+export const UPPERCASE_TRANSFORMER = (fragments, separator) => {
+  return fragments.join(separator).toUpperCase()
 }
 
-/* Prepare the global instance and export it */
+/* Converts a string into a slug */
 
-const urlSlug = new UrlSlug()
-const global = urlSlug.convert.bind(urlSlug)
+export function convert(string, options = {}) {
+  const {
+    camelCase = true,
+    separator = '-',
+    transformer = LOWERCASE_TRANSFORMER,
+  } = parseOptions(options)
 
-global.UrlSlug = UrlSlug
-global.convert = global
-global.revert = urlSlug.revert.bind(urlSlug)
-global.transformers = defaultTransformers /* TODO Deprecate in v3 */
+  const fragments = String(string)
+    .normalize('NFKD')
+    .replace(COMBINING_CHARS, '')
+    .match(camelCase ? CONVERT_CAMELCASE : CONVERT)
 
-module.exports = global
+  if (!fragments) {
+    return ''
+  }
+
+  return transformer
+    ? transformer(fragments, separator)
+    : fragments.join(separator)
+}
+
+/* Reverts a slug back to a string */
+
+export function revert(slug, options = {}) {
+  const {
+    camelCase = false,
+    separator,
+    transformer = false
+  } = parseOptions(options)
+
+  let fragments
+
+  /* Determine which method will be used split the slug */
+
+  if ('' === separator) {
+    fragments = camelCase
+      ? String(slug).match(REVERT_CAMELCASE_WITH_EMPTY_SEPARATOR)
+      : [String(slug)]
+  } else if ('string' === typeof separator) {
+    fragments = String(slug).split(separator)
+  } else {
+    fragments = String(slug).match(camelCase ? REVERT_CAMELCASE : REVERT)
+  }
+
+  if (!fragments) {
+    return ''
+  }
+
+  return transformer ? transformer(fragments, ' ') : fragments.join(' ')
+}
+
+/* Sets convert() as the default export */
+
+export default convert
--- test/index.js
+++ test/index.js
@@ -1,164 +1,86 @@
-const expect = require('chai').expect
-
-const urlSlug = require('../src/index')
+import { expect } from 'chai'
+import * as urlSlug from '../src'
 
 describe('module', () => {
-  it('includes UrlSlug constructor as a property', () => {
-    expect(urlSlug.UrlSlug.constructor)
-      .to.be.equal(urlSlug.constructor)
-  })
-
-  it('has a convert and a revert method', () => {
-    expect(urlSlug.convert)
-      .to.be.a('function')
-    expect(urlSlug.revert)
-      .to.be.a('function')
+  it('sets the convert function as the default export', () => {
+    expect(urlSlug.convert).to.be.equal(urlSlug.default)
   })
 
   it('has builtin transformers', () => {
-    expect(urlSlug.transformers)
-      .to.be.deep.equal({
-        lowercase: urlSlug.UrlSlug.LOWERCASE_TRANSFORMER,
-        uppercase: urlSlug.UrlSlug.UPPERCASE_TRANSFORMER,
-        titlecase: urlSlug.UrlSlug.TITLECASE_TRANSFORMER,
-      })
+    expect(typeof urlSlug.LOWERCASE_TRANSFORMER).to.be.equal('function')
+    expect(typeof urlSlug.TITLECASE_TRANSFORMER).to.be.equal('function')
+    expect(typeof urlSlug.UPPERCASE_TRANSFORMER).to.be.equal('function')
   })
 
   it('has a working lowercase transformer', () => {
-    expect(urlSlug.transformers.lowercase(['TEST', 'STRING'], ' '))
-      .to.be.equal('test string')
+    const fragments = ['AA', 'BB']
+    const separator = '-'
+    const transformer = urlSlug.LOWERCASE_TRANSFORMER
+    expect(transformer(fragments, separator)).to.be.equal('aa-bb')
+  })
+
+  it('has a working tittle case transformer', () => {
+    const fragments = ['aA', 'bB']
+    const separator = '-'
+    const transformer = urlSlug.TITLECASE_TRANSFORMER
+    expect(transformer(fragments, separator)).to.be.equal('Aa-Bb')
   })
 
   it('has a working uppercase transformer', () => {
-    expect(urlSlug.transformers.uppercase(['test', 'string'], ' '))
-      .to.be.equal('TEST STRING')
+    const fragments = ['aa', 'bb']
+    const separator = '-'
+    const transformer = urlSlug.UPPERCASE_TRANSFORMER
+    expect(transformer(fragments, separator)).to.be.equal('AA-BB')
   })
-
-  it('has a working titlecase transformer', () => {
-    expect(urlSlug.transformers.titlecase(['tesT', 'strinG'], ' '))
-      .to.be.equal('Test String')
-  })
-
-  it('calls convert if called as a function', () => {
-    expect(urlSlug)
-      .to.be.equal(urlSlug.convert)
-  })
-})
-
-describe('constructor', () => {
-  it('contains builtin transformers as static properties', () => {
-    expect(urlSlug.UrlSlug.LOWERCASE_TRANSFORMER)
-      .to.be.a('function')
-    expect(urlSlug.UrlSlug.UPPERCASE_TRANSFORMER)
-      .to.be.a('function')
-    expect(urlSlug.UrlSlug.TITLECASE_TRANSFORMER)
-      .to.be.a('function')
-  })
-
-  it('has default options set', () => {
-    const instance = new urlSlug.UrlSlug()
-    expect(instance.camelCase)
-      .to.be.true
-    expect(instance.separator)
-      .to.be.string('-')
-    expect(instance.transformer)
-      .to.be.equal(urlSlug.UrlSlug.LOWERCASE_TRANSFORMER)
-  })
-
-  it('sets properties with options argument values', () => {
-    const transformer = () => {}
-    const instance = new urlSlug.UrlSlug({
-      camelCase: false,
-      separator: '_',
-      transformer,
-    })
-    expect(instance.camelCase)
-      .to.be.false
-    expect(instance.separator)
-      .to.be.string('_')
-    expect(instance.transformer)
-      .to.be.equal(transformer)
-  })
-})
-
-describe('validate', () => {
-  it('accepts an options object', () => {
-    expect(() => new urlSlug.UrlSlug({}))
-      .to.not.throw()
-  })
-
-  it('accepts only boolean values in camelCase option', () => {
-    expect(() => new urlSlug.UrlSlug({ camelCase: true }))
-     .to.not.throw()
-     expect(() => new urlSlug.UrlSlug({ camelCase: false }))
-      .to.not.throw()
-    expect(() => new urlSlug.UrlSlug({ camelCase: null }))
-     .to.throw('camelCase must be a boolean')
-  })
-
-  it('accepts an empty string as separator', () => {
-    expect(() => new urlSlug.UrlSlug({ separator: '' }))
-      .to.not.throw()
-  })
-
-  it('accepts only unreserved characters in RFC 3986 as separator', () => {
-    expect(() => new urlSlug.UrlSlug({ separator: '-._~' }))
-      .to.not.throw()
-    expect(() => new urlSlug.UrlSlug({ separator: '+' }))
-      .to.throw('separator has invalid characters')
-  })
-
-  it('does not accept a separator that is not a string', () => {
-    expect(() => new urlSlug.UrlSlug({ separator: 123 }))
-      .to.throw('separator must be a string')
-  })
-
-  it('accepts false as a transformer', () => {
-    expect(() => new urlSlug.UrlSlug({ transformer: false }))
-      .to.not.throw()
-  })
-
-  it('accepts a function as a transformer', () => {
-    expect(() => new urlSlug.UrlSlug({ transformer: () => {} }))
-      .to.not.throw()
-  })
-
-  it('accepts builtin functions as a transformer', () => {
-    const lowercase = urlSlug.UrlSlug.LOWERCASE_TRANSFORMER
-    const uppercase = urlSlug.UrlSlug.UPPERCASE_TRANSFORMER
-    const titlecase = urlSlug.UrlSlug.TITLECASE_TRANSFORMER
-    expect(() => new urlSlug.UrlSlug({ transformer: lowercase }))
-      .to.not.throw()
-    expect(() => new urlSlug.UrlSlug({ transformer: uppercase }))
-      .to.not.throw()
-    expect(() => new urlSlug.UrlSlug({ transformer: titlecase }))
-      .to.not.throw()
-  })
-
-  it('accepts only false or a function as a transformer', () => {
-    expect(() => new urlSlug.UrlSlug({ transformer: true }))
-      .to.throw('transformer must be a function')
-    expect(() => new urlSlug.UrlSlug({ transformer: 'string' }))
-      .to.throw('transformer must be a function')
-    expect(() => new urlSlug.UrlSlug({ transformer: {} }))
-      .to.throw('transformer must be a function')
-  })
-
-  it('throws a deprecation error for tranformer strings', () => {
-    expect(() => new urlSlug.UrlSlug({ transformer: 'uppercase' }))
-      .to.throw('Using transformer name as string was deprecated')
-  }) // TODO Remove in v3
 })
 
 describe('convert', () => {
-  it('uses validate to check options', () => {
-    expect(() => urlSlug.convert('', { separator: 123 }))
-      .to.throw('separator must be a string')
+  it('accepts only boolean values in camelCase option', () => {
+    expect(() => urlSlug.convert('', { camelCase: true }))
+      .to.not.throw()
+    expect(() => urlSlug.convert('', { camelCase: false }))
+      .to.not.throw()
     expect(() => urlSlug.convert('', { camelCase: null }))
       .to.throw('camelCase must be a boolean')
   })
 
-  it('converts input to string', () => {
+  it('accepts an empty string as separator', () => {
+    expect(() => urlSlug.convert('', { separator: '' }))
+      .to.not.throw()
+  })
+
+  it('accepts only unreserved characters in RFC 3986 as separator', () => {
+    expect(() => urlSlug.convert('', { separator: '-._~' }))
+      .to.not.throw()
+    expect(() => urlSlug.convert('', { separator: '+' }))
+      .to.throw('separator has an invalid character')
+  })
+
+  it('does not accept a separator that is not a string', () => {
+    expect(() => urlSlug.convert('', { separator: 123 }))
+      .to.throw('separator must be a string')
+  })
+
+  it('accepts false as a transformer', () => {
+    expect(() => urlSlug.convert('', { transformer: false }))
+      .to.not.throw()
+  })
+
+  it('accepts a function as a transformer', () => {
+    expect(() => urlSlug.convert('', { transformer: () => {} }))
+      .to.not.throw()
+  })
+
+  it('accepts only false or a function as a transformer', () => {
+    expect(() => urlSlug.convert('', { transformer: true }))
+      .to.throw('transformer must be false or a function')
+    expect(() => urlSlug.convert('', { transformer: 'string' }))
+      .to.throw('transformer must be false or a function')
+    expect(() => urlSlug.convert('', { transformer: {} }))
+      .to.throw('transformer must be false or a function')
+  })
+
+  it('casts input to string', () => {
     expect(urlSlug.convert(123))
       .to.be.equal('123')
   })
@@ -169,80 +91,42 @@ describe('convert', () => {
   })
 
   it('removes accents', () => {
-    expect(urlSlug.convert('á é í ó ú'))
-      .to.be.equal('a-e-i-o-u')
+    expect(urlSlug.convert('á é í ó ú ç áéíóúç'))
+      .to.be.equal('a-e-i-o-u-c-aeiouc')
   })
 
-  it('uses upper case as transformer and use the default separator', () => {
-    expect(urlSlug.convert('a bronx tale', urlSlug.transformers.uppercase))
-      .to.be.equal('A-BRONX-TALE')
-  }) // TODO v3 deprecate
-
-  it('uses upper case as transformer and use the default separator', () => {
-    const options = { transformer: urlSlug.transformers.uppercase }
+  it('uses uppercase transformer and the default separator', () => {
+    const options = { transformer: urlSlug.UPPERCASE_TRANSFORMER }
     expect(urlSlug.convert('a bronx tale', options))
       .to.be.equal('A-BRONX-TALE')
   })
 
-  it('uses underscore as separator and title case as transformer', () => {
-    expect(urlSlug.convert('tom jobim', '_', urlSlug.transformers.titlecase))
-      .to.be.equal('Tom_Jobim')
-  }) // TODO v3 deprecate
-
-  it('uses underscore as separator and title case as transformer', () => {
+  it('uses uppercase transformer and underscore as separator', () => {
     const options = {
       separator: '_',
-      transformer: urlSlug.transformers.titlecase
+      transformer: urlSlug.TITLECASE_TRANSFORMER,
     }
     expect(urlSlug.convert('tom jobim', options))
       .to.be.equal('Tom_Jobim')
   })
 
-  it('allows multiple characters as separator and maintains the case', () => {
-    expect(urlSlug.convert('Charly García', '-._~-._~', false))
-      .to.be.equal('Charly-._~-._~Garcia')
-  }) // TODO v3 deprecate
-
-  it('allows multiple characters as separator and maintains the case', () => {
-    const options = {
-      separator: '-._~-._~',
-      transformer: false
-    }
+  it('uses multiple characters as separator and maintains the case', () => {
+    const options = { separator: '-._~-._~', transformer: false }
     expect(urlSlug.convert('Charly García', options))
       .to.be.equal('Charly-._~-._~Garcia')
   })
 
   it('returns a camel case slug', () => {
-    expect(urlSlug.convert('java script', '', urlSlug.transformers.titlecase))
-      .to.be.equal('JavaScript')
-  }) // TODO v3 deprecate
-
-  it('returns a camel case slug', () => {
     const options = {
       separator: '',
-      transformer: urlSlug.transformers.titlecase
+      transformer: urlSlug.TITLECASE_TRANSFORMER
     }
     expect(urlSlug.convert('java script', options))
       .to.be.equal('JavaScript')
   })
 
   it('splits a camel case string', () => {
-    expect(urlSlug.convert('javaScript'))
-      .to.be.equal('java-script')
-    expect(urlSlug.convert('javaSCRIPT', null, null))
-      .to.be.equal('java-SCRIPT')
-    expect(urlSlug.convert('JAVAScript', null, null))
-      .to.be.equal('JAVA-Script')
-    expect(urlSlug.convert('jaVAScriPT', null, null))
-      .to.be.equal('ja-VA-Scri-PT')
-    expect(urlSlug.convert('JaVaScriPt', null, null))
-      .to.be.equal('Ja-Va-Scri-Pt')
-    expect(urlSlug.convert('JaVaScrIpT', null, null))
-      .to.be.equal('Ja-Va-Scr-Ip-T')
-  }) // TODO v3 deprecate
-
-  it('splits a camel case string', () => {
-    const options = { separator: null, transformer: null }
+    const options = { transformer: false }
     expect(urlSlug.convert('javaScript'))
       .to.be.equal('java-script')
     expect(urlSlug.convert('javaSCRIPT', options))
@@ -261,14 +145,6 @@ describe('convert', () => {
     expect(urlSlug.convert('javaScript', { camelCase: false }))
       .to.be.equal('javascript')
   })
-
-  it('returns only consonants', () => {
-    const transform = (fragments, separator) => fragments
-      .join(separator)
-      .replace(/[aeiou]/gi, '')
-    expect(urlSlug.convert('React', '', transform))
-      .to.be.equal('Rct')
-  }) // TODO v3 deprecate
 
   it('returns only consonants', () => {
     const options = {
@@ -293,14 +169,52 @@ describe('convert', () => {
 })
 
 describe('revert', () => {
-  it('uses validate to check options', () => {
-    expect(() => urlSlug.revert('', { separator: 123 }))
-      .to.throw('separator must be a string')
+  it('accepts only boolean values in camelCase option', () => {
+    expect(() => urlSlug.revert('', { camelCase: true }))
+      .to.not.throw()
+    expect(() => urlSlug.revert('', { camelCase: false }))
+      .to.not.throw()
     expect(() => urlSlug.revert('', { camelCase: null }))
       .to.throw('camelCase must be a boolean')
   })
 
-  it('converts input to string', () => {
+  it('accepts an empty string as separator', () => {
+    expect(() => urlSlug.revert('', { separator: '' }))
+      .to.not.throw()
+  })
+
+  it('accepts only unreserved characters in RFC 3986 as separator', () => {
+    expect(() => urlSlug.revert('', { separator: '-._~' }))
+      .to.not.throw()
+    expect(() => urlSlug.revert('', { separator: '+' }))
+      .to.throw('separator has an invalid character')
+  })
+
+  it('does not accept a separator that is not a string', () => {
+    expect(() => urlSlug.revert('', { separator: 123 }))
+      .to.throw('separator must be a string')
+  })
+
+  it('accepts false as a transformer', () => {
+    expect(() => urlSlug.revert('', { transformer: false }))
+      .to.not.throw()
+  })
+
+  it('accepts a function as a transformer', () => {
+    expect(() => urlSlug.revert('', { transformer: () => {} }))
+      .to.not.throw()
+  })
+
+  it('accepts only false or a function as a transformer', () => {
+    expect(() => urlSlug.revert('', { transformer: true }))
+      .to.throw('transformer must be false or a function')
+    expect(() => urlSlug.revert('', { transformer: 'string' }))
+      .to.throw('transformer must be false or a function')
+    expect(() => urlSlug.revert('', { transformer: {} }))
+      .to.throw('transformer must be false or a function')
+  })
+
+  it('casts input to string', () => {
     expect(urlSlug.revert(123))
       .to.be.equal('123')
   })
@@ -311,17 +225,18 @@ describe('revert', () => {
   })
 
   it('splits a camel case slug', () => {
-    expect(urlSlug.revert('javaScript', { camelCase: true }))
+    const options = { camelCase: true }
+    expect(urlSlug.revert('javaScript', options))
       .to.be.equal('java Script')
-    expect(urlSlug.revert('javaSCRIPT', { camelCase: true }))
+    expect(urlSlug.revert('javaSCRIPT', options))
       .to.be.equal('java SCRIPT')
-    expect(urlSlug.revert('JAVAScript', { camelCase: true }))
+    expect(urlSlug.revert('JAVAScript', options))
       .to.be.equal('JAVA Script')
-    expect(urlSlug.revert('jaVAScriPT', { camelCase: true }))
+    expect(urlSlug.revert('jaVAScriPT', options))
       .to.be.equal('ja VA Scri PT')
-    expect(urlSlug.revert('JaVaScriPt', { camelCase: true }))
+    expect(urlSlug.revert('JaVaScriPt', options))
       .to.be.equal('Ja Va Scri Pt')
-    expect(urlSlug.revert('JaVaScrIpT', { camelCase: true }))
+    expect(urlSlug.revert('JaVaScrIpT', options))
       .to.be.equal('Ja Va Scr Ip T')
   })
 
@@ -331,30 +246,19 @@ describe('revert', () => {
   })
 
   it('splits on camel case and convert input to upper case', () => {
-    const slug = 'ClaudioBaglioni_is-NOT-German'
-    expect(urlSlug.revert(slug, '', urlSlug.transformers.uppercase))
-      .to.be.equal('CLAUDIO BAGLIONI_IS-NOT-GERMAN')
-  }) // TODO v3 deprecate
-
-  it('splits on camel case and convert input to upper case', () => {
     const options = {
+      camelCase: true,
       separator: '',
-      transformer: urlSlug.transformers.uppercase
+      transformer: urlSlug.UPPERCASE_TRANSFORMER,
     }
     expect(urlSlug.revert('ClaudioBaglioni_is-NOT-German', options))
       .to.be.equal('CLAUDIO BAGLIONI_IS-NOT-GERMAN')
   })
 
   it('returns the title of a Pink Floyd track', () => {
-    const slug = 'comfortably-._~numb'
-    expect(urlSlug.revert(slug, '-._~', urlSlug.transformers.titlecase))
-      .to.be.equal('Comfortably Numb')
-  }) // TODO v3 deprecate
-
-  it('returns the title of a Pink Floyd track', () => {
     const options = {
       separator: '-._~',
-      transformer: urlSlug.transformers.titlecase
+      transformer: urlSlug.TITLECASE_TRANSFORMER
     }
     expect(urlSlug.revert('comfortably-._~numb', options))
       .to.be.equal('Comfortably Numb')
