12a3b3787307da121c9a13aa664017730e4bf969
stldo <st.shared@icloud.com>
2020-07-25 23:27:20 -0300

    Improvement: remove Array.map usage to reduce bundle size

--- src/transformers.js
+++ src/transformers.js
@@ -8,9 +8,13 @@ export const SENTENCECASE_TRANSFORMER = (fragments, separator) => {
 }
 
 export const TITLECASE_TRANSFORMER = (fragments, separator) => {
-  return fragments.map(fragment => {
-    return fragment.charAt(0).toUpperCase() + fragment.slice(1).toLowerCase()
-  }).join(separator)
+  const buffer = []
+  for (const fragment of fragments) {
+    buffer.push(
+      fragment.charAt(0).toUpperCase() + fragment.slice(1).toLowerCase()
+    )
+  }
+  return buffer.join(separator)
 }
 
 export const UPPERCASE_TRANSFORMER = (fragments, separator) => {
